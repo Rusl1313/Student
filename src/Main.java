7557baadc2e346811b8b81dac3e5ae73177fc349
rusik <rusikss.ss13@gmail.com>
2023-03-21 12:55:20 +0300

    5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        Students students = new Students();
-        students.
-
+        Student student = new Student();
+        student.study();
+        JavaStudent javaStudent = new JavaStudent();
+        javaStudent.study();
     }
+    public static class Student {
+        protected final String studying;
+
+        protected Student(String work) {
+            this.studying = work;
+        }
+
+        public Student() {
+            this.studying = "Прохожу тестовое задание.";
+        }
+
+        public void study() {
+            System.out.println("Я очень занят. " + studying);
+        }
+    }
+
+    public static class JavaStudent extends Student {
+        public JavaStudent() {
+            super("Прохожу курс Java");
+        }
+    }
+
 }
